245154a4e48f23e27d9c727bf7dfb847f3e66348
CosmeValera <cosmevalerareales@gmail.com>
2022-05-31 11:31:49 +0200

    create folders account, compose, repository and search. Navbar-smp now redirects from compose to the others and vice versa

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, 'compose/compose.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
