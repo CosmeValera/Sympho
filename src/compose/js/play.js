71c0a594d21589f50928f8273a495f14abacbe97
CosmeValera <cosmevalerareales@gmail.com>
2022-05-31 16:35:56 +0200

    an already made melody is played when play button is clicked

--- src/compose/js/play.js
+++ src/compose/js/play.js
@@ -1,5 +1,23 @@
-var synth = new Tone.PolySynth().toMaster();
+const synth = new Tone.Synth().toDestination();
 
-divPlay.addEventListener('click', function() {
-    synth.triggerAttackRelease('C4', '8n');
-});
+const notes = [
+    { pitch: "C", accidental: "bb", octave: "4", duration: 4 },
+    { pitch: "D", accidental: "###", octave: "4", duration: 8 },
+    { pitch: "E", accidental: "", octave: "4", duration: 8 },
+    { pitch: "F", accidental: "", octave: "4", duration: 2 },
+];
+
+function play() {
+  console.log("HERE")
+    let delay = Tone.now();
+    for(let i = 0; i < notes.length; i++) {
+      let waitingForNextNote = 4/notes[i].duration;
+        if (i>0) {
+          let noteValue = 4/notes[i-1].duration;
+          delay += noteValue;
+        }
+        synth.triggerAttackRelease(notes[i].pitch + notes[i].accidental + notes[i].octave, waitingForNextNote, delay);  
+    }
+}
+
+divPlay.addEventListener('click', play);
